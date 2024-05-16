ac3e51e5abeff0febd9f96ec27c53a0a0cf3a69c
Basmala Mohamed Zain El Abidine <102691187+BasmalaMohamed46@users.noreply.github.com>
2024-05-16 17:39:16 +0300

    Lab 4

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,18 @@ class Article < ApplicationRecord
     include Visible
   
     has_many :comments, dependent: :destroy
-  
+    belongs_to :user 
+    has_one_attached :image
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
+    def archive_if_reports_exceeded
+      if reports_count >= 3 && !archived?
+        puts "Article #{id} reached : #{reports_count} reports, it will be archived."
+        update(archived: true)
+      end
+    end
+    def report
+      increment!(:reports_count)
+    end
   end
   
